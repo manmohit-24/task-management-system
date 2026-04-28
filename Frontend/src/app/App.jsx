1839fa9c96c88d136d1c23a7b06ba8267cebcc14
Manmohit Singh <manmohit249@gmail.com>
2026-04-29 02:18:54 +0530

    chore(files): move App.jsx and routes to /app and moved assets to /public

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { store } from "../store/store.js";
 
 const App = () => {
     return (
