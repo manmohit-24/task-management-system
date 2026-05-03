50d5bc30b9ca0190ef61b983e2f4edb8f219a3b0
Manmohit Singh <manmohit249@gmail.com>
2026-05-04 01:34:05 +0530

    Revert "chore(files): move App.jsx and routes to /app and moved assets to /public"
    This reverts commit 1acac58213548179fafcf4c76a7b21bcb58d6ff3.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppRouter from "./app/routes";
-
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthPage , HomePage , Dashboard} from "./pages/index.js";
 import "./Themes/Light.css";
 // import "./Themes/Dark.css";
 import "./Themes/Mutual.css";
 import "./utils/Icons.css";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/home",
+                element: <HomePage />,
+            },
+            {
+                path: "auth/:authType",
+                element: <AuthPage />,
+            },
+            {
+                path: "app/:pageType/:tagId",
+                element: <Dashboard />,
+            },
+        ],
+    },
+]);
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <AppRouter />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
