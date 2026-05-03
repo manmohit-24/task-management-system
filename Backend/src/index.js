4a164d49614aecaa414d8d18f3a551777a15ddc2
Manmohit Singh <manmohit249@gmail.com>
2026-05-04 01:34:05 +0530

    Revert "feat(CORS): add CORS origins at backend for client"
    This reverts commit d2679e67e56d028bcf827eef0597e6834afbe9bc.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,11 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 import { logger } from "./utils/logger.js";
 import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+});
 
 logger.seperator();
 
@@ -9,11 +13,17 @@ const port = process.env.PORT;
 
 // ******************** Connecting to DataBase and app ****************
 connectDB()
-    .then(() => {
-        app.listen(port, () => {
-            logger.success("Express connected", `Running at port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.log("APP CONNECTION FAILED", err);
+.then( () => {
+    app.listen(port, () => {
+    logger.success("Express connected", `Running at port ${port}`);
     });
+})
+.catch((err) => {
+    console.log('APP CONNECTION FAILED' , err)
+ })
+
+
+
+
+
+ 
