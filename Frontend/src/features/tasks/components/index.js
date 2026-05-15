5fa52f0821592d5e1ad60b3e5d4f6e77780f0925
Manmohit Singh <manmohit249@gmail.com>
2026-05-16 00:22:08 +0530

    fix(ui): fix sidebar - body coordination and merge sidebarHeader to sidebar

--- Frontend/src/features/tasks/components/index.js
+++ Frontend/src/features/tasks/components/index.js
@@ -1,8 +1,7 @@
 import Sidebar from "./Sidebar/Sidebar";
 import SidebarLabel from "./SidebarLabel/SidebarLabel";
-import SidebarHeader from "./SidebarHeader/SidebarHeader";
 import AddProjectDropDown from "./AddProjectDropDown/AddProjectDropDown";
 import ProfileMenu from "./ProfileMenu/ProfileMenu";
 import Toolbar from "./Toolbar/Toolbar";
 
-export { Sidebar, SidebarHeader, SidebarLabel, AddProjectDropDown, ProfileMenu, Toolbar };
+export { Sidebar, SidebarLabel, AddProjectDropDown, ProfileMenu, Toolbar };
--- Frontend/src/features/tasks/layout/TasksLayout.jsx
+++ Frontend/src/features/tasks/layout/TasksLayout.jsx
@@ -1,13 +1,40 @@
 import styles from "./TasksLayout.module.css";
 import { Sidebar, Toolbar } from "../components";
+import { useState } from "react";
 
 export default function TasksLayout() {
+    const [isSidebarExpanded, setIsSidebarExpanded] = useState(() => {
+        const storedValue = localStorage.getItem("isSidebarExpanded");
+        if (storedValue === null) return true;
+        return storedValue === "true";
+    });
+
+    const onToggleSidebarExpansion = () => {
+        setIsSidebarExpanded((p) => {
+            localStorage.setItem("isSidebarExpanded", String(!p));
+            return !p;
+        });
+    };
+
     return (
-        <div className={styles.page}>
-            <Sidebar />
-            <main className={styles.body}>
-                <Toolbar />
-                {/* Page Component Here */}
+        <div
+            className={`${styles.page} ${
+                isSidebarExpanded ? styles.sidebarExpanded : styles.sidebarCollapsed
+            }`}
+        >
+            <aside className={styles.sidebar}>
+                <Sidebar
+                    isSidebarExpanded={isSidebarExpanded}
+                    onToggleSidebarExpansion={onToggleSidebarExpansion}
+                />
+            </aside>
+
+            <main className={styles.content}>
+                <div className={styles.contentInner}>
+                    <Toolbar />
+
+                    <section className={styles.pageContent}></section>
+                </div>
             </main>
         </div>
     );
